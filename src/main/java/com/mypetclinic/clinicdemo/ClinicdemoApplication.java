e940092299ccb528da4d54a6a5c9215c99e95bf1
Radu Radu <neoradu@gmail.com>
2022-07-26 18:28:44 +0300

    my first pet clinic commit
    Signed-off-by: Radu Radu <neoradu@gmail.com>

---
+++ src/main/java/com/mypetclinic/clinicdemo/ClinicdemoApplication.java
@@ -0,0 +1,13 @@
+package com.mypetclinic.clinicdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mypetclinic/clinicdemo/ClinicdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypetclinic.clinicdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
