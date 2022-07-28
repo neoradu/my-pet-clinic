bde78823bed4cf22a8d0d8a3f60d01762d31c8ab
Radu Radu <neoradu@gmail.com>
2022-07-28 15:21:02 +0300

    Refactor service interfaces. Closes #11
    Signed-off-by: Radu Radu <neoradu@gmail.com>

--- pet-clinic-data/src/main/java/com/mypetclinic/clinicdemo/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/mypetclinic/clinicdemo/services/OwnerService.java
@@ -1,20 +1,13 @@
 package com.mypetclinic.clinicdemo.services;
 
-import java.util.Set;
-
 import com.mypetclinic.clinicdemo.model.Owner;
 
 /**
  * This is a service between controllers and the data sources.
  * This will help easily change the data source.
  * */
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
 	Owner findByLastName(String lastName);
-	
-	Owner findById(Long id);
-	
-	Owner save(Owner owner);
-	
-	Set<Owner> findAll();
+
 }
