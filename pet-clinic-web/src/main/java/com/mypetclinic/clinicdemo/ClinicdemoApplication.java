1b8b0229ba13162b0d2dce2bd2ab2546a4aafce6
Radu Radu <neoradu@gmail.com>
2022-07-28 10:28:12 +0300

    multi package build seems to work.no tests. closes #1
    Signed-off-by: Radu Radu <neoradu@gmail.com>

--- pet-clinic-web/src/main/java/com/mypetclinic/clinicdemo/ClinicdemoApplication.java
+++ pet-clinic-web/src/main/java/com/mypetclinic/clinicdemo/ClinicdemoApplication.java
@@ -1,6 +1,7 @@
 package com.mypetclinic.clinicdemo;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
