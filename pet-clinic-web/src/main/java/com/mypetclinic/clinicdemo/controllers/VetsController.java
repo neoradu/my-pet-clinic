304798e1813d9aef434a37e885e00923000c48f4
Radu Radu <neoradu@gmail.com>
2022-07-29 16:26:54 +0300

    add vets list.Closes #15
    Signed-off-by: Radu Radu <neoradu@gmail.com>

--- pet-clinic-web/src/main/java/com/mypetclinic/clinicdemo/controllers/VetsController.java
+++ pet-clinic-web/src/main/java/com/mypetclinic/clinicdemo/controllers/VetsController.java
@@ -4,11 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mypetclinic.clinicdemo.services.VetService;
+
 @Controller
 public class VetsController {
 	
+	private final VetService vetService;
+	
+	public VetsController(VetService vetService) {
+		super();
+		this.vetService = vetService;
+	}
+
 	@RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
 	public String index(Model model) {
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
 	}
 
