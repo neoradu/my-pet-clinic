15f83c69016a88b243b209f6bd623c2acc957e95
Radu Radu <neoradu@gmail.com>
2022-07-29 16:17:03 +0300

    list all owners on the owners page.Closes #16
    Signed-off-by: Radu Radu <neoradu@gmail.com>

--- pet-clinic-web/src/main/java/com/mypetclinic/clinicdemo/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/com/mypetclinic/clinicdemo/controllers/OwnersController.java
@@ -4,11 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mypetclinic.clinicdemo.services.OwnerService;
+
 @Controller
 public class OwnersController {
+	final OwnerService ownersService;
 	
+	public OwnersController(OwnerService ownersService) {
+		super();
+		this.ownersService = ownersService;
+	}
+
 	@RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
 	public String index(Model model) {
+		model.addAttribute("owners", ownersService.findAll());
 		return "owners/index";
 	}
 
