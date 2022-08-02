63704938c9bd51485ab6fe1941cd93bbae60c050
Radu Radu <neoradu@gmail.com>
2022-08-02 12:52:20 +0300

    add Specility entity.Closes #27
    Signed-off-by: Radu Radu <neoradu@gmail.com>

---
+++ pet-clinic-data/src/main/java/com/mypetclinic/clinicdemo/model/Speciality.java
@@ -0,0 +1,15 @@
+package com.mypetclinic.clinicdemo.model;
+
+public class Speciality extends BaseEntity {
+	private String desciption;
+
+	public String getDesciption() {
+		return desciption;
+	}
+
+	public void setDesciption(String desciption) {
+		this.desciption = desciption;
+	}
+	
+
+}
