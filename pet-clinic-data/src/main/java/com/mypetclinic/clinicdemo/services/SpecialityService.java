0f4924c37a5c59c4d4fd0d0fe7dd1ac70ac160b6
Radu Radu <neoradu@gmail.com>
2022-08-02 15:50:50 +0300

    implemet speciality service map.Closes #31
    Signed-off-by: Radu Radu <neoradu@gmail.com>

---
+++ pet-clinic-data/src/main/java/com/mypetclinic/clinicdemo/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package com.mypetclinic.clinicdemo.services;
+
+import com.mypetclinic.clinicdemo.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/mypetclinic/clinicdemo/services/map/SpecialityServiceMap.java
@@ -0,0 +1,9 @@
+package com.mypetclinic.clinicdemo.services.map;
+
+import com.mypetclinic.clinicdemo.model.Speciality;
+import com.mypetclinic.clinicdemo.services.SpecialityService;
+
+public class SpecialityServiceMap  extends AbstractMapService<Speciality, Long> 
+                                   implements SpecialityService {
+
+}
