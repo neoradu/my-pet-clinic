35102692324781bcde8e0967bfbba3d78d8ec6f9
Radu Radu <neoradu@gmail.com>
2022-08-09 19:46:43 +0300

    Create visit Entity.Closes #38
    Signed-off-by: Radu Radu <neoradu@gmail.com>

--- pet-clinic-data/src/main/java/com/mypetclinic/clinicdemo/model/Visit.java
+++ pet-clinic-data/src/main/java/com/mypetclinic/clinicdemo/model/Visit.java
@@ -2,10 +2,23 @@ package com.mypetclinic.clinicdemo.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 	
+	@Column(name = "visit_date")
 	private LocalDate date;
+	
 	private String description;
+	
+	@ManyToOne
+	@JoinColumn(name = "pet_id")
 	private Pet pet;
 	
 	public LocalDate getDate() {
