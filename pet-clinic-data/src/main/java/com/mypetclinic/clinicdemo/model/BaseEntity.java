59cc79017f3472d98372e96c18ee59664b7dc793
Radu Radu <neoradu@gmail.com>
2022-08-09 18:57:28 +0300

    implement base entity.Closes #35
    Signed-off-by: Radu Radu <neoradu@gmail.com>

--- pet-clinic-data/src/main/java/com/mypetclinic/clinicdemo/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/mypetclinic/clinicdemo/model/BaseEntity.java
@@ -2,7 +2,19 @@ package com.mypetclinic.clinicdemo.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass 
+//Tells JPA that this is a class that other classes will inherit from
+//--> tells not to create a special table for this class in the database
 public class BaseEntity implements Serializable {
+	@Id//tels JPA that this is the index of the tabble
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	//IDENTITY -> Indicates that the persistence provider must assign primary keys
+	//for the entity using a database identity column
 	private Long id;
 
 	public Long getId() {
