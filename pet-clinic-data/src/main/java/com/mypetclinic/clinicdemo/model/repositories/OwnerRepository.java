33f55de4686bac1d8150c5b57d69e20abde2b7e0
Radu Radu <neoradu@gmail.com>
2022-08-15 14:39:44 +0300

    add Owner Spring data JPA service.Closes #40
    Signed-off-by: Radu Radu <neoradu@gmail.com>

--- pet-clinic-data/src/main/java/com/mypetclinic/clinicdemo/model/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/mypetclinic/clinicdemo/model/repositories/OwnerRepository.java
@@ -1,10 +1,20 @@
 package com.mypetclinic.clinicdemo.model.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.mypetclinic.clinicdemo.model.Owner;
 
 //Because Owner is annotated as a JPA entity, spring data JPA will provide  an implementation at the runtime
-public interface OwnerRepository extends CrudRepository<Owner, Long>{
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
+	
+	//Querry method. Spring data will parse the method name and deduce what parameter
+	//needs to look for in the required object(in this case Owner.lastName).
+	//After that will automatically generate a implementation for this interface method definition
+	// And use hibernate to fetch data from the data base.
+	//One of the coolest features of Spring data JBA
+	//https://www.tutorialspoint.com/java8/java8_optional_class.htm
+	public Optional<Owner> findByLastName(String lastName);
 
 }
---
+++ pet-clinic-data/src/main/java/com/mypetclinic/clinicdemo/services/springdatajpa/OwnerSDJpaService.java
@@ -0,0 +1,68 @@
+package com.mypetclinic.clinicdemo.services.springdatajpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import com.mypetclinic.clinicdemo.model.Owner;
+import com.mypetclinic.clinicdemo.model.repositories.OwnerRepository;
+import com.mypetclinic.clinicdemo.model.repositories.PetRepository;
+import com.mypetclinic.clinicdemo.model.repositories.PetTypeRepository;
+import com.mypetclinic.clinicdemo.services.OwnerService;
+
+@Service
+@Profile("springdatajpa")
+public class OwnerSDJpaService implements OwnerService {
+	
+	final private OwnerRepository ownerRepository;
+	final private PetRepository petRepository;
+	final private PetTypeRepository petTypeRepository;
+	public OwnerSDJpaService(OwnerRepository ownerRepository, PetRepository petRepository,
+			                 PetTypeRepository petTypeRepository) {
+		super();
+		this.ownerRepository = ownerRepository;
+		this.petRepository = petRepository;
+		this.petTypeRepository = petTypeRepository;
+	}
+
+	@Override
+	public Owner findById(Long id) {
+		return ownerRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Owner save(Owner object) {
+		return ownerRepository.save(object);
+	}
+
+	@Override
+	public Set<Owner> findAll() {
+		Set<Owner> result = new HashSet<>();
+		//https://www.geeksforgeeks.org/double-colon-operator-in-java/
+		//Lambda expression
+		//ownerRepository.findAll().forEach( o -> result.add(o));
+		//Double colon operator
+		ownerRepository.findAll().forEach(result::add);
+		return result;
+	}
+
+	@Override
+	public void delete(Owner object) {
+		ownerRepository.delete(object);
+		
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		ownerRepository.deleteById(id);
+	}
+
+	@Override
+	public Owner findByLastName(String lastName) {
+		
+		return ownerRepository.findByLastName(lastName).orElse(null);
+	}
+
+}
