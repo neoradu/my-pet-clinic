1127b611d0e6c42cc06d220a06ef9afc9a20f5b3
Radu Radu <neoradu@gmail.com>
2022-08-17 11:29:08 +0300

    Upgrade to Junit5.Closes #4
    Signed-off-by: Radu Radu <neoradu@gmail.com>

--- pet-clinic-web/src/test/java/com/mypetclinic/clinicdemo/ClinicdemoApplicationTests.java
+++ pet-clinic-web/src/test/java/com/mypetclinic/clinicdemo/ClinicdemoApplicationTests.java
@@ -1,16 +1,42 @@
 package com.mypetclinic.clinicdemo;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
+
+@ExtendWith(SpringExtension.class)//Lunit5 syntax
 @SpringBootTest
 public class ClinicdemoApplicationTests {
 
+	@BeforeAll//Executed before all tests
+	public void generalInit() {
+		
+	}
+	
+	@BeforeEach// Executed before each test
+	public void setUp() {
+		
+	}
+    @AfterEach//Executed after each test --> cleanup
+    public void clean() {
+    	
+    }
+	
     @Test
     public void contextLoads() {
+    }
+    
+
+    
+    @AfterAll//Executed after all tests
+    public void generalCleanUp() {
+    	
     }
 
 }
