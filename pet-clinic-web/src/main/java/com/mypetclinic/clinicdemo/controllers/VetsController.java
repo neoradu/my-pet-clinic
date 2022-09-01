484da2c8bd2927e236601036febc88d6e9623f77
Radu Radu <neoradu@gmail.com>
2022-09-01 10:49:56 +0300

    Add vets api endpoint.Closes #60
    Signed-off-by: Radu Radu <neoradu@gmail.com>

--- pet-clinic-web/src/main/java/com/mypetclinic/clinicdemo/controllers/VetsController.java
+++ pet-clinic-web/src/main/java/com/mypetclinic/clinicdemo/controllers/VetsController.java
@@ -1,9 +1,13 @@
 package com.mypetclinic.clinicdemo.controllers;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.mypetclinic.clinicdemo.model.Vet;
 import com.mypetclinic.clinicdemo.services.VetService;
 
 @Controller
@@ -16,10 +20,16 @@ public class VetsController {
 		this.vetService = vetService;
 	}
 
-	@RequestMapping({"/vets", "/vets/index", "/vets/index.html","vets.html"})
+	@GetMapping({"/vets", "/vets/index", "/vets/index.html","vets.html"})
 	public String index(Model model) {
 		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
+	}
+	
+	@GetMapping("/api/vets")
+	public @ResponseBody Set<Vet> getVetsJson() {
+		// this is by default parsed by jackson to JSON
+		return vetService.findAll();
 	}
 
 }
